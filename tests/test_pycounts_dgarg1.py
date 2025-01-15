e19c0e49a038b93c7c219a67f30b2b3a7c04d8e4
Dhruv Garg <63773208+vrudhgarg@users.noreply.github.com>
2025-01-15 14:34:37 -0800

    unit tests done

--- tests/test_pycounts_dgarg1.py
+++ tests/test_pycounts_dgarg1.py
@@ -1,5 +1,8 @@
-from pycounts_dgarg1.pycounts_dgarg1 import count_words
+from pycounts_dgarg1.gg import count_words
 from collections import Counter
+from pycounts_dgarg1.plotting import plot_words 
+import matplotlib    
+import pytest
 
 def test_count_words():
     """Test word counting from a file."""
@@ -9,3 +12,22 @@ def test_count_words():
                         'different': 1, 'results': 1})
     actual = count_words("tests/einstein.txt")
     assert actual == expected, "Einstein quote counted incorrectly!"
+
+def test_plot_words(): 
+    """Test plotting of word counts."""
+    counts = Counter({'insanity': 1, 'is': 1, 'doing': 1, 
+                      'the': 1, 'same': 1, 'thing': 1, 
+                      'over': 2, 'and': 2, 'expecting': 1,
+                      'different': 1, 'results': 1})
+    fig = plot_words(counts)
+    assert isinstance(fig, matplotlib.container.BarContainer), \
+           "Wrong plot type"
+    assert len(fig.datavalues) == 10, \
+           "Incorrect number of bars plotted"
+    
+
+def test_plot_words_error(): 
+    """Check TypeError raised when Counter not used."""
+    with pytest.raises(TypeError):
+        list_object = ["Pythons", "are", "non", "venomous"]
+        plot_words(list_object)
