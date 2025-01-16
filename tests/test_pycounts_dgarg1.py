1c3f28d0708cc402e1c638a8a1744514c5e0f77c
Dhruv Garg <63773208+vrudhgarg@users.noreply.github.com>
2025-01-15 16:51:19 -0800

    changed

--- tests/test_pycounts_dgarg1.py
+++ tests/test_pycounts_dgarg1.py
@@ -4,26 +4,25 @@ from pycounts_dgarg1.plotting import plot_words
 import matplotlib    
 import pytest
 
-def test_count_words():
+@pytest.fixture         
+def einstein_counts(): 
+    return Counter({'insanity': 1, 'is': 1, 'doing': 1, 
+                    'the': 1, 'same': 1, 'thing': 1, 
+                    'over': 2, 'and': 2, 'expecting': 1,
+                    'different': 1, 'results': 1})
+
+def test_count_words(einstein_counts):  
     """Test word counting from a file."""
-    expected = Counter({'insanity': 1, 'is': 1, 'doing': 1, 
-                        'the': 1, 'same': 1, 'thing': 1, 
-                        'over': 2, 'and': 2, 'expecting': 1,
-                        'different': 1, 'results': 1})
+    expected = einstein_counts
     actual = count_words("tests/einstein.txt")
     assert actual == expected, "Einstein quote counted incorrectly!"
 
-def test_plot_words(): 
+def test_plot_words(einstein_counts):   
     """Test plotting of word counts."""
-    counts = Counter({'insanity': 1, 'is': 1, 'doing': 1, 
-                      'the': 1, 'same': 1, 'thing': 1, 
-                      'over': 2, 'and': 2, 'expecting': 1,
-                      'different': 1, 'results': 1})
-    fig = plot_words(counts)
+    fig = plot_words(einstein_counts)
     assert isinstance(fig, matplotlib.container.BarContainer), \
            "Wrong plot type"
-    assert len(fig.datavalues) == 10, \
-           "Incorrect number of bars plotted"
+    assert len(fig.datavalues) == 10, "Incorrect number of bars plotted"
     
 
 def test_plot_words_error(): 
@@ -31,3 +30,13 @@ def test_plot_words_error():
     with pytest.raises(TypeError):
         list_object = ["Pythons", "are", "non", "venomous"]
         plot_words(list_object)
+
+def test_integration():
+    """Test count_words() and plot_words() workflow."""
+    counts = count_words("tests/einstein.txt")
+    fig = plot_words(counts)
+    assert isinstance(fig, matplotlib.container.BarContainer), \
+           "Wrong plot type"
+    assert len(fig.datavalues) == 10, \
+           "Incorrect number of bars plotted"
+    assert max(fig.datavalues) == 2, "Highest word count should be 2"
